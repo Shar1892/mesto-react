046547a2cdf8902dee020388393fb6879daeec2a
sg.sharov <sg.sharov@tensor.ru>
2021-04-27 00:36:09 +0300

    fix: поправил апи, убрал лишние импорты

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext, useRef } from "react";
+import { useRef } from "react";
 import PopupWithForm from './PopupWithForm';
 
 const EditAvatarPopup = ({isOpen, onClose, onUpdateAvatar}) => {
